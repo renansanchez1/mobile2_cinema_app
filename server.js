4098bb74ec9497511cc4faea3e0f252556828c32
Renan Sanchez <renansanchez0501@gmail.com>
2025-10-28 17:42:14 -0400

    ajuste porta

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/dados', (req, res) => {
   })
 })
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando em http://0.0.0.0:${PORT}`)
 })
